5998079512b55a34affa5f3de03ded50f69cfb08
Aleksandr Gaakh <gaach006@mail.ru>
2020-01-27 22:47:57 +0300

    поправил логику

--- src/game/GameBoard.java
+++ src/game/GameBoard.java
@@ -4,23 +4,26 @@ public class GameBoard {
 
     String[][] arr;
 
-    public GameBoard(){
+    public GameBoard() {
+        resetOrNewArray();
+    }
+
+    public void resetOrNewArray() {
         arr = new String[3][3];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 arr[i][j] = ("[ ]");
             }
         }
-//        showArray(arr);
-
     }
+
     public void showArray(String[][] a) {
-       for (int i = 0; i < a.length; i++){
-           for (int j = 0; j < a[i].length; j++){
-               System.out.print(a[i][j]);
-           }
-           System.out.println();
-       }
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
 
